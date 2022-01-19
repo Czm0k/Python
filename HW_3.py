c8e67002a7ac83c4302fa5f90c0ef2af0b7034d9
Czm0k <volknik99@mail.rugit config --global user.name Czm0k>
2022-01-19 22:20:46 +0300

    changes HW_3

--- HW_3.py
+++ HW_3.py
@@ -91,25 +91,29 @@ currency_convertor = item_2
 # Сделать if в котором будет условие: если currency_convertor, то выполнять следующие шаги задания, иначе, вывести в консоль (“Переменная currency_convertor = ”, item_3)
 if currency_convertor:
     currency_usd = usd_item
-    target_currency = byn_item
+    target_currency = (input("Enter the name of the currency: "))
     target_currency_amount = 50
     currency_result = 0
     if target_currency == 'eur':
-        currency_result = target_currency_amount / usd_eur_rate
+        currency_result = round(target_currency_amount / usd_eur_rate, 2)
         print(target_currency_amount, eur_item, "=", currency_result, usd_item)
     elif target_currency == 'uah':
-        currency_result = target_currency_amount / usd_uah_rate
+        currency_result = round(target_currency_amount / usd_uah_rate, 2)
         print(target_currency_amount, uah_item, "=", currency_result, usd_item)
     elif target_currency == 'chf':
-        currency_result = target_currency_amount / usd_chf_rate
+        currency_result = round(target_currency_amount / usd_chf_rate, 2)
         print(target_currency_amount, chf_item, "=", currency_result, usd_item)
     elif target_currency == 'rub':
-        currency_result = target_currency_amount / usd_rub_rate
+        currency_result = round(target_currency_amount / usd_rub_rate, 2)
         print(target_currency_amount, rub_item, "=", currency_result, usd_item)
     elif target_currency == 'byn':
-        currency_result = target_currency_amount / usd_byn_rate
+        currency_result = round(target_currency_amount / usd_byn_rate, 2)
         print(target_currency_amount, byn_item, "=", currency_result, usd_item)
+    elif target_currency == 'usd':
+        currency_result = round(target_currency_amount / usd_usd_rate, 2)
+        print(target_currency_amount, usd_item, "=", currency_result, usd_item)
     else:
         print("Unknow currency")
+
 else:
     print("Переменная currency_convertor = ", item_3)
